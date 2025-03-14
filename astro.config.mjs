348740a58a7e1be75a6284ef85eee90b4c4978e7
Scott Johnson <scottj@thegrovery.com>
2025-03-14 14:54:02 -0400

    adding adapter for netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   integrations: [tailwind({
     applyBaseStyles: false
   })],
   output: 'server',
+  adapter: netlify(),
   vite: {
     envPrefix: 'HUBSPOT_'
   }
